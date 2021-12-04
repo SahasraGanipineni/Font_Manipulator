38a7f2d21fd7a8b42d9b9e1912bc4dc7729d0164
SahasraGanipineni <87746901+SahasraGanipineni@users.noreply.github.com>
2021-12-04 11:27:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(500 , 500); 
@@ -7,7 +11,6 @@ function setup(){
     canvas.position(1000 , 150);
 
     poseNet = ml5.poseNet(video , modelLoaded);
-
     poseNet.on('pose' , gotPoses);
 }
 
@@ -19,5 +22,18 @@ function gotPoses(results){
     if(results.length > 0)
     {
         console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("Left Wrist X = " + leftWristX , "Right Wrist X = " + rightWristX , "Difference = " + difference);
     }
 } 
+
+function draw() {
+    document.getElementById('font_size').innerHTML = "Font size is - " + difference + "px";
+
+    textSize(difference);
+    fill('white');
+    text('Ok' , 0 , 300);
+}
